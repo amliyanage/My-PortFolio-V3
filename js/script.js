ba2afc1c8646c2df1ba34d96b0b316011d710cc4
amliyanage <ashenmliyanage@gmail.com>
2024-04-24 22:47:43 -0700

    set auto typing effect and slide show

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings : ["Ashen Madhushanka","A Software Engineer", "A Web Designer", "A Mobile App Developer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+});
